65abe7334c7038e62121e4c1ce4ad53430476102
cloud866 <dklein927@gmail.com>
2023-03-01 17:53:42 +0300

    add Comparator

---
+++ src/CarComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+public class CarComparator implements Comparator<Car> {
+
+    @Override
+    public int compare(Car car1, Car car2) {
+        return car1.getName().compareTo(car2.getName());
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -70,20 +70,27 @@ public class Main {
         carList.add(car29);
         carList.add(car30);
 
-        Map<Car, Integer> carMap = new HashMap<>();
+        Set<Car> carSet = new TreeSet<>(new CarComparator().reversed());
+        carSet.addAll(carList);
 
-        for (Car car : carList) {
-            if (carMap.containsKey(car)) {
-                int value = carMap.get(car);
-                carMap.put(car, ++value);
-            } else {
-                carMap.put(car, 1);
-            }
+        for (Car car : carSet) {
+            System.out.println(car.getName());
         }
 
-        for (Map.Entry<Car, Integer> carEntry : carMap.entrySet()) {
-            System.out.println(carEntry.getKey() + "=" + carEntry.getValue());
-        }
+//        Map<Car, Integer> carMap = new HashMap<>();
+//
+//        for (Car car : carList) {
+//            if (carMap.containsKey(car)) {
+//                int value = carMap.get(car);
+//                carMap.put(car, ++value);
+//            } else {
+//                carMap.put(car, 1);
+//            }
+//        }
+//
+//        for (Map.Entry<Car, Integer> carEntry : carMap.entrySet()) {
+//            System.out.println(carEntry.getKey() + "=" + carEntry.getValue());
+//        }
 
 //        Set<Car> carSet = new HashSet<>(carList);
 //
